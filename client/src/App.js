6d8df40d9aeb057c75331d991c7ffdafcbabb3c8
Jeremy Du <97752691+Razieldu@users.noreply.github.com>
2023-05-05 17:38:23 +0800

    加入 react-spinner  使用graphql內建的loading變數來操控

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import { useEffect, useState } from "react";
 import { gql } from "@apollo/client";
 import "./App.css";
 import client from "./apollo.js";
+import { BarLoader } from 'react-spinners';
 function App() {
+  const [loading, setLoading] = useState(true);
   const [resultdata, setData] = useState([]);
+  const [error,setError] =useState(false)
   // const [apiState, setApiState] = useState("name");
   const [inputValue, setInputValue] = useState("");
   // client
@@ -23,7 +26,7 @@ function App() {
   //   .catch((error) => console.log(error));
   let defaultApi="name"
   const fetchData = async (key) => {
-    const { data } = await client.query({
+    const { loading: queryLoading, error,data } = await client.query({
       query: gql`
         query {
           userdata {
@@ -34,6 +37,8 @@ function App() {
     });
     // console.log(errors)
     setData(data.userdata);
+    setLoading(queryLoading)
+    setError(error)
   };
   const handleInputValue = (event) => {
     setInputValue(event.target.value);
@@ -41,33 +46,39 @@ function App() {
 
   const handleAPI = (event) => {
     event.preventDefault()
+    setLoading(true)
     if(inputValue==="") return
     fetchData(inputValue);
   };
 
   useEffect(() => {
     fetchData(defaultApi);
-  }, []);
+  }, [defaultApi]);
+
+
   return (
-    <div>
-      <form onSubmit={handleAPI}>
-        <input
-          value={inputValue}
-          onChange={handleInputValue}
-        />
-        <button >search</button>
-      </form>
-      <ul>
+    <div style={{position:"relative",display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center"}}>
+    <form onSubmit={handleAPI}>
+      <input value={inputValue} onChange={handleInputValue} />
+      <button>Search</button>
+    </form>
+
+    {loading ? (
+      <div className="loading-container"style={{height:"100vh",display:"flex",alignItems:"center",position:"absolute",zIndex:"100",top:"50%"}} >
+        <BarLoader color="#36D7B7" loading={loading} />
+      </div>
+    ) : (
+      <div>
         {resultdata.map((each) => {
-          // console.log(each.id)
           for (let key in each) {
             if (key === "__typename") continue;
             return <h1>{each[key]}</h1>;
           }
         })}
-        {/* {data} */}
-      </ul>
-    </div>
+      </div>
+    )}
+    {error && <p>Error {error.message}</p>}
+  </div>
   );
 }
 
